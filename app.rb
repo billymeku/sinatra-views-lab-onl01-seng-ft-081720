e71e4d12b8925a28d25e44b6841cb5e39e3c092f
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-29 01:18:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@ class App < Sinatra::Base
 	get '/hello' do
 		erb :hello
 	end
-#   get '/goodbye.erb' do
-# 		erb :goodbye
-# 	end
+  get '/goodbye.erb' do
+		erb :goodbye
+	end
 
 #   get '/date' do
 #   		erb :date 
